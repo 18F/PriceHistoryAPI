41da4755090359dd87d633c5e02d2f1e5a90f35b
Robert L. Read <read.robert@gmail.com>
2013-09-02 21:20:04 -0400

    More robust handling of empty searches

--- PPBottleApp.py
+++ PPBottleApp.py
@@ -11,6 +11,21 @@ app = Bottle()
 def does_authenticate(user,password):
     return (user == MasterUsername and password == MasterPassword)
 
+def convertPSCToLegalPattern(str):
+    if (str is None) or (str == 'None') or (str == ''):
+        return '*';
+    else:
+        return str;
+
+# this needs to be improved, but basically,
+# we don't want them to do a blank search.
+# it could be prevented at the GUI layer as well.
+def convertSearchStringToLegalPattern(str):
+    if (str is None) or (str == 'None') or (str == ''):
+        return 'nothing at all, choose something';
+    else:
+        return str;
+
 @app.route('/apisolr',method='POST')
 def apisolr():
     user = request.forms.get('user')
@@ -19,6 +34,8 @@ def apisolr():
         return template('BadAuthentication')        
     search_string = request.forms.get('search_string')
     psc_pattern = request.forms.get('psc_pattern')
+    search_string = convertSearchStringToLegalPattern(search_string);
+    psc_pattern = convertPSCToLegalPattern(psc_pattern);
     print "APISOLR search_string" + search_string
     print "PSCSOLR search_string" + psc_pattern
     return searchApiSolr(URLToSolr,PathToDataFiles,search_string,psc_pattern)
